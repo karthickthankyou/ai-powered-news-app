faa17ad1abfe0ddff7c3af5933ec439ba3270d42
Karthick Ragavendran <karthickthankyou@gmail.com>
2024-03-10 22:05:06 +0530

    Setup authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { prisma } from '@/db'
+import { UserButton } from '@clerk/nextjs'
 export default async function Home() {
   const users = await prisma.user.findMany()
   return (
     <main>
+      <UserButton />
       {users.map((user) => (
         <div key={user.id} className="p-4">
           <div>{user.id}</div>
